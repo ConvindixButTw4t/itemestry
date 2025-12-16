7bcc243a440b19b735ddc3f57f141b421405e4d4
Tw4t <terntovich@gmail.com>
2025-12-16 15:15:20 +0500

    clean and setup all files

---
+++ src/main/java/net/tw4t/itemestry/Itemestry.java
@@ -0,0 +1,34 @@
+package net.tw4t.itemestry;
+
+import org.slf4j.Logger;
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(Itemestry.MODID)
+public class Itemestry
+    {
+    public static final String MODID = "itemestry";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public Itemestry(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {}
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {}
+
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {}
+}
